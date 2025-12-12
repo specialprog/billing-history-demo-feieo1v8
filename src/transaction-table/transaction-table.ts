39fb0adc3981efc77c6b4a3f9d93258d98120e31
specialprog <tr2prog@bellsouth.net>
2025-12-12 03:53:58 -0500

    added pagination

--- src/transaction-table/transaction-table.ts
+++ src/transaction-table/transaction-table.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, signal } from '@angular/core';
 import { Transaction } from '../billing/models/transaction.model';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 
@@ -15,6 +15,40 @@ export class TransactionTableComponent {
 
   viewTransaction(tx: Transaction) {
     this.selectedTransaction = tx;
+  }
+
+    // Pagination signals
+  pageSize = signal<number>(5);
+  currentPage = signal<number>(1);
+
+  // Compute total pages
+  totalPages = computed(() =>
+    Math.ceil(this.transactions.length / this.pageSize())
+  );
+
+  // Slice transactions for current page
+  pagedTransactions = computed(() => {
+    const start = (this.currentPage() - 1) * this.pageSize();
+    const end = start + this.pageSize();
+    return this.transactions.slice(start, end);
+  });
+
+  goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages()) {
+      this.currentPage.set(page);
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage() < this.totalPages()) {
+      this.currentPage.update(p => p + 1);
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage() > 1) {
+      this.currentPage.update(p => p - 1);
+    }
   }
 
   
