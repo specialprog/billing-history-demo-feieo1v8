bf840cfcb4fa06b060b861668b00a75178d80b6d
specialprog <tr2prog@bellsouth.net>
2025-12-12 03:37:20 -0500

    added unit test

--- src/billing/billing.service.spec.ts
+++ src/billing/billing.service.spec.ts
@@ -54,3 +54,20 @@ describe('BillingService', () => {
     expect(result[0].id).toBe('1');
   }));
 });
+
+/* tslint:disable:no-unused-variable */
+
+// import { TestBed, inject } from '@angular/core/testing';
+// import { BillingService } from './billing.service';
+
+// describe('BillingService', () => {
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       providers: [BillingService]
+//     });
+//   });
+
+//   it('should ...', inject([BillingService], (service: BillingService) => {
+//     expect(service).toBeTruthy();
+//   }));
+// });
